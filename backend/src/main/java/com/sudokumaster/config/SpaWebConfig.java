16c5f8521ddf2fa9e62a69ed3b4c062d0b456041
klu2300033213 <bhanuprakash.gandham12@gmail.com>
2026-07-24 21:14:39 +0530

    Disable Spring Boot internal JVM resource caching in SpaWebConfig to force instant production static asset updates

--- backend/src/main/java/com/sudokumaster/config/SpaWebConfig.java
+++ backend/src/main/java/com/sudokumaster/config/SpaWebConfig.java
@@ -3,6 +3,7 @@ package com.sudokumaster.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -16,7 +17,8 @@ public class SpaWebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
-                .resourceChain(true)
+                .setCacheControl(CacheControl.noCache().mustRevalidate())
+                .resourceChain(false)
                 .addResolver(new PathResourceResolver() {
                     @Override
                     protected Resource getResource(String resourcePath, Resource location) throws IOException {
